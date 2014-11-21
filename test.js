59da6b92320a85049c970a3f7ebc64cbe1736270
Dirk Gadsden <dirk@esherido.com>
2014-11-20 23:08:00 -0500

    Start building type system

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 
 var fs     = require('fs'),
     Parser = require('./lib/parser'),
-    parser = new Parser()
+    parser = new Parser(),
+    TypeSystem = require('./lib/typesystem').TypeSystem
 
 var code  = fs.readFileSync('./examples/fibonacci.hb').toString()
 var input = "let a: Type = (2 + 3) + 4"
 
 var tree = parser.parse(code)
-// console.log(tree.statement.rvalue)
-if (tree) tree.print();
+// if (tree) tree.print();
+
+
+var ts = new TypeSystem()
+ts.walk(tree)
