2ad841838bc8d3fa8058cbcfde8265d7b768173f
Dirk Gadsden <dirk@esherido.com>
2015-03-31 12:57:35 -0400

    Add a few classes tests

--- test/test-system.js
+++ test/test-system.js
@@ -18,31 +18,59 @@ function runCompiledCode (tree) {
   return eval("(function(){\n"+tree.compile()+"\n})()")
 }
 
-vows.describe('Hummingbird').addBatch({
-  'System': {
-    'given a for loop program': {
-      topic: parseAndWalk(programs.forLoop, 'for-loop.hb'),
-      'should parse': function (tree) {
+describe('System', function () {
+  describe('given a for loop program', function () {
+    var tree = null
+    it('should parse', function () {
+      tree = parseAndWalk(programs.forLoop)
+      expect(tree).to.be.ok()
+    })
+    it('should produce the right result when executed', function () {
+      var result = runCompiledCode(tree)
+      expect(result).to.eql(10)
+    })
+  })
+
+  describe('given a class', function () {
+    describe('with an invalid let-property', function () {
+      var source = "var a = func () -> Integer { return 1 }\n"+
+                   "class B { var c: () -> Integer = a }\n"
+      it('should fail to parse', function () {
+        expect(function () {
+          parseAndWalk(source)
+        }).to.throwException(/non-literal default for property/)
+      })
+    })
+
+    describe('with a valid definition', function () {
+      var source = "class A {\n"+
+                   "  var b: Integer = 1\n"+
+                   "  init () { this.b = this.b + 1 }\n"+
+                   "  func c () -> Integer { return this.b + 1 }\n"+
+                   "}\n"+
+                   "var a = new A()\n"+
+                   "return a.c()\n"
+      var tree = null
+      it('should parse', function () {
+        tree = parseAndWalk(source)
         expect(tree).to.be.ok()
-      },
-      'when executed': {
-        topic: function (tree) { return runCompiledCode(tree) },
-        'should produce the right result': function (result) {
-          expect(result).to.eql(10)
-        }
-      }
-    },
-    'given a while-true program': {
-      topic: parseAndWalk(programs.whileTrue, 'while-true.hb'),
-      'should parse': function (tree) {
-        expect(tree).to.be.ok()
-      },
-      'when executed': {
-        topic: function (tree) { return runCompiledCode(tree) },
-        'should produce the right result': function (result) {
-          expect(result).to.eql(5)
-        }
-      }
-    }
-  }
-}).export(module)
+      })
+      it('should product the correct result', function () {
+        var result = runCompiledCode(tree)
+        expect(result).to.eql(3)
+      })
+    })
+  })
+
+  xdescribe('given a while-true program', function () {
+    var tree = null
+    it('should parse', function () {
+      tree = parseAndWalk(programs.whileTrue)
+      expect(tree).to.be.ok()
+    })
+    it('should produce the correct result when executed', function () {
+      var result = runCompiledCode(tree)
+      expect(result).to.eql(5)
+    })
+  })
+})
