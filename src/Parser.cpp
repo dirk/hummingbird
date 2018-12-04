68f256e173df6a52d3c09328855a97b7dc0cff91
Dirk Gadsden <dirk@esherido.com>
2018-12-03 19:13:32 -0800

    Use variants for parsing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,20 +1,68 @@
 #include "Parser.h"
 
-PRoot Parser::parse() {
+PRoot* Parser::parse() {
   return parseRoot();
 }
 
-PRoot Parser::parseRoot() {
-  auto root = PRoot();
+PVar Parser::parseVarStatement(int token) {
+  auto var = PVar();
+  expect(T_IDENTIFIER);
+  expect(T_EQUALS);
+  return var;
+}
+
+PNode Parser::parseStatement(int token) {
+  switch (token) {
+    case T_VAR:
+      return PNode(parseVarStatement(token));
+  }
+  fatalTokenError(token);
+  return PNode();
+}
+
+PRoot* Parser::parseRoot() {
+  auto root = new PRoot();
 
   while (true) {
     auto token = lexer->yylex();
-    if (token == 0) {
-      break;
+    switch (token) {
+      case T_EOF:
+        goto eof;
+      default:
+        auto node = parseStatement(token);
+        if (node.isUnknown()) {
+          fatalNodeError(node);
+        }
+        root->nodes.push_back(node);
+        break;
     }
-
-    std::cout << token << std::endl;
   }
+  eof:
 
   return root;
+}
+
+token_t Parser::expect(token_t expected) {
+  auto token = lexer->yylex();
+  if (token != expected) {
+    std::cerr
+      << "Unexpected token: expected " << expected
+      << ", got " << token
+      << std::endl;
+    exit(1);
+  }
+  return token;
+}
+
+void Parser::fatalNodeError(PNode node) {
+  std::cerr << "Unmatched node" << std::endl;
+  exit(1);
+}
+
+void Parser::fatalTokenError(token_t t) {
+  std::cerr
+    << "Unrecognized token " << t
+    << " at line " << lexer->lineno()
+    << std::endl;
+  exit(1);
 }
--- src/Parser.h
+++ src/Parser.h
@@ -1,31 +1,67 @@
 #pragma once
 
 #include <istream>
+#include <string>
+#include <variant>
 #include <vector>
 
 #include "Lexer.h"
 
-class PNode {};
+class PVar {
+public:
+  PVar() { };
 
-class PRoot : PNode
-{
+  ~PVar() {
+    if (identifier) {
+      delete identifier;
+    }
+  }
+
+  std::string* identifier = nullptr;
+};
+
+typedef struct {} PUnknown;
+
+class PNode {
+public:
+  PNode(PVar var) : node(var) { };
+  PNode() : node((PUnknown){}) { };
+
+  bool isUnknown() {
+    return std::holds_alternative<PUnknown>(node);
+  }
+
 private:
+  std::variant<
+    PVar,
+    PUnknown
+  > node;
+};
+
+class PRoot {
+public:
   std::vector<PNode> nodes;
 };
 
-class Parser
-{
+class Parser {
 public:
-  Parser(std::istream *input) : lexer(new yyFlexLexer(input)) { };
+  Parser(std::istream* input) : lexer(new yyFlexLexer(input)) { };
 
   ~Parser() {
     delete lexer;
   }
 
-  PRoot parse();
+  PRoot* parse();
 
 private:
-  yyFlexLexer *lexer;
+  yyFlexLexer* lexer;
 
-  PRoot parseRoot();
+  PRoot* parseRoot();
+  PNode parseStatement(token_t token);
+  PVar parseVarStatement(token_t token);
+
+  token_t expect(token_t token);
+
+  void fatalNodeError(PNode node);
+  void fatalTokenError(token_t t);
 };
