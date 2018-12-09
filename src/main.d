47c106e091f902d82337f03c1bfd11f3950a598f
Dirk Gadsden <dirk@esherido.com>
2018-12-08 16:02:42 -0800

    Start using new parser

--- src/main.d
+++ src/main.d
@@ -5,6 +5,8 @@ import std.stdio : File, writeln;
 static import ast.compiler;
 static import ir.compiler;
 static import target.bytecode.printer;
+
+import parser.new_parser : Parser;
 import parser = parser.parser;
 
 void main(string[] args) {
@@ -15,7 +17,8 @@ void main(string[] args) {
 
   auto source = readText!string(args[1]);
 
-  auto program = parser.parse(source, true);
+  // auto program = parser.parse(source, true);
+  auto program = new Parser(source).parseProgram();
   writeln("AST");
   writeln(program.toPrettyString());
 
