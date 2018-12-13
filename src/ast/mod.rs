05c15f363283f2adc97de2efa244c96fce987abb
Dirk Gadsden <dirk@esherido.com>
2018-12-12 23:14:09 -0800

    Start defining AST nodes

---
+++ src/ast/mod.rs
@@ -0,0 +1,3 @@
+pub mod nodes;
+
+pub use self::nodes::*;
---
+++ src/ast/nodes.rs
@@ -0,0 +1,94 @@
+use std::boxed::Box;
+
+use super::super::parser::Token;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Node {
+    Assignment(Assignment),
+    Block(Block),
+    Identifier(Identifier),
+    Infix(Infix),
+    Integer(Integer),
+    PostfixProperty(PostfixProperty),
+    Program(Program),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Assignment {
+    pub lhs: Box<Node>,
+    pub rhs: Box<Node>,
+}
+
+impl Assignment {
+    pub fn new(lhs: Node, rhs: Node) -> Self {
+        Self {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Block {
+    pub nodes: Vec<Node>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Identifier {
+    pub value: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Integer {
+    pub value: i64,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum InfixOp {
+    Add,
+    Multiply,
+    Subtract,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Infix {
+    pub lhs: Box<Node>,
+    pub op: InfixOp,
+    pub rhs: Box<Node>,
+}
+
+impl Infix {
+    pub fn new(lhs: Node, token: Token, rhs: Node) -> Self {
+        let op = match token {
+            Token::Minus => InfixOp::Subtract,
+            Token::Plus => InfixOp::Add,
+            Token::Star => InfixOp::Multiply,
+            _ => unreachable!(),
+        };
+        Self {
+            lhs: Box::new(lhs),
+            op,
+            rhs: Box::new(rhs),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct PostfixProperty {
+    target: Box<Node>,
+    value: String,
+}
+
+impl PostfixProperty {
+    pub fn new(target: Node, value: String) -> Self {
+        Self {
+            target: Box::new(target),
+            value: value,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Program {
+    pub nodes: Vec<Node>,
+}
