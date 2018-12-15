390c5b64b19e04e62116c0f7d82e52fd5c970573
Dirk Gadsden <dirk@esherido.com>
2018-12-14 19:57:59 -0800

    Make `main()` actually parse and print an AST

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::process::exit;
+use std::{env, fs};
+
 mod ast;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 || args.len() > 2 {
+        println!("Usage: hummingbird [file]");
+        exit(-1);
+    }
+    let filename = &args[1];
+    let source = fs::read_to_string(filename).expect("Unable to read source file");
+
+    let program = parser::parse(source);
+    println!("Program:\n{:?}", program);
 }
