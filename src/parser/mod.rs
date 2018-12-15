edd9efd6091cfd7a1eebe11d74605215b1e7590e
Dirk Gadsden <dirk@esherido.com>
2018-12-14 19:57:39 -0800

    Add public parsing interface

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,17 @@
+use super::ast::{Node, Program};
+
 mod lexer;
 mod location;
 mod parser;
 
 pub use self::lexer::Token;
 pub use self::location::Location;
+
+pub fn parse<I: ToString>(input: I) -> Program {
+    let mut token_stream = lexer::TokenStream::from_string(input.to_string());
+    let node = parser::parse_program(&mut token_stream);
+    match node {
+        Node::Program(program) => program,
+        _ => panic!("Not a Program: {:?}", node),
+    }
+}
