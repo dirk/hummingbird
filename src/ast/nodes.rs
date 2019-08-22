a2256de416ae401c2e1d40212b4282484f623ad8
Dirk Gadsden <dirk@esherido.com>
2019-08-21 19:09:50 -0700

    Remove some old stuff

--- src/ast/nodes.rs
+++ src/ast/nodes.rs
@@ -1,7 +1,4 @@
 use std::boxed::Box;
-use std::collections::HashSet;
-use std::fmt::{Display, Error, Formatter};
-use std::path::PathBuf;
 
 use super::super::parser::{Location, Span, Token};
 
@@ -21,28 +18,6 @@ pub enum Node {
     Return(Return),
     String(StringLiteral),
     Var(Var),
-}
-
-impl Display for Node {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        use Node::*;
-        match self {
-            Assignment(_) => f.write_str("Assignment"),
-            Block(_) => f.write_str("Block"),
-            Function(_) => f.write_str("Function"),
-            Identifier(_) => f.write_str("Identifier"),
-            Import(_) => f.write_str("Import"),
-            Infix(_) => f.write_str("Infix"),
-            Integer(_) => f.write_str("Integer"),
-            Let(_) => f.write_str("Let"),
-            Module(_) => f.write_str("Module"),
-            PostfixCall(_) => f.write_str("PostfixCall"),
-            PostfixProperty(_) => f.write_str("PostfixProperty"),
-            Return(_) => f.write_str("Return"),
-            String(_) => f.write_str("String"),
-            Var(_) => f.write_str("Var"),
-        }
-    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -70,13 +45,6 @@ pub struct Function {
     pub name: Option<String>,
     pub body: Box<Node>,
     pub location: Option<Location>,
-    /// The variables of this function which must be stored in the `Closure`
-    /// on the heap.
-    bindings: Option<HashSet<String>>,
-    /// The variables that this function depends on its parent closure to
-    /// provide. To ensure nested closure forwarding a `Closure` will be
-    /// created if this is present, even if `bindings` is not present.
-    parent_bindings: Option<HashSet<String>>,
 }
 
 impl Function {
@@ -94,139 +62,6 @@ impl Function {
             name,
             body,
             location: None,
-            bindings,
-            parent_bindings,
-        }
-    }
-
-    pub fn has_bindings(&self) -> bool {
-        self.bindings.is_some()
-    }
-
-    pub fn get_bindings(&self) -> Option<HashSet<String>> {
-        self.bindings.clone()
-    }
-
-    pub fn has_parent_bindings(&self) -> bool {
-        self.parent_bindings.is_some()
-    }
-
-    pub fn get_parent_bindings(&self) -> Option<HashSet<String>> {
-        self.parent_bindings.clone()
-    }
-}
-
-fn none_if_empty(set: HashSet<String>) -> Option<HashSet<String>> {
-    if set.is_empty() {
-        None
-    } else {
-        Some(set)
-    }
-}
-
-// FIXME: Flip this visitor on its head and make it a top-down walk to
-//   discover variables, their usages (closures), and catch undefined
-//   variables early.
-
-/// Detect which locals need to be bound into a closure for functions within
-/// this function. Also detect which variables this functions depends on a
-/// parent closure for.
-///
-/// Returns a 2-tuple with `bound` and `parent_bound`.
-fn detect_bindings(body: &Node) -> (Option<HashSet<String>>, Option<HashSet<String>>) {
-    // Keep track of `let` and `var` declarations as we go.
-    let mut locals = HashSet::new();
-    // Locals which are bound by functions within.
-    let mut bindings = HashSet::new();
-    // Variables we depend on our parent for.
-    let mut parent_bindings = HashSet::new();
-    detect_bindings_visitor(body, &mut locals, &mut bindings, &mut parent_bindings);
-    (none_if_empty(bindings), none_if_empty(parent_bindings))
-}
-
-fn detect_bindings_visitor(
-    node: &Node,
-    locals: &mut HashSet<String>,
-    bindings: &mut HashSet<String>,
-    parent_bindings: &mut HashSet<String>,
-) {
-    // Using macros to make things more concise and avoid extra allocations.
-    macro_rules! identify {
-        ($i:expr) => {{
-            if !locals.contains($i) {
-                parent_bindings.insert($i.clone());
-            }
-        }};
-    }
-    macro_rules! visit {
-        ($x:expr) => {
-            detect_bindings_visitor($x, locals, bindings, parent_bindings)
-        };
-    }
-    match node {
-        Node::Assignment(assignment) => {
-            visit!(&assignment.lhs);
-            visit!(&assignment.rhs);
-        }
-        Node::Block(block) => {
-            for node in block.nodes.iter() {
-                visit!(node);
-            }
-        }
-        Node::Function(function) => {
-            if let Some(name) = &function.name {
-                locals.insert(name.clone());
-            }
-            if let Some(nested_parent_bindings) = &function.parent_bindings {
-                for nested_parent_binding in nested_parent_bindings.iter() {
-                    if locals.contains(nested_parent_binding) {
-                        bindings.insert(nested_parent_binding.clone());
-                    } else {
-                        parent_bindings.insert(nested_parent_binding.clone());
-                    }
-                }
-            }
-        }
-        Node::Identifier(identifier) => {
-            identify!(&identifier.value);
-        }
-        Node::Import(_) => {}
-        Node::Infix(infix) => {
-            visit!(&infix.lhs);
-            visit!(&infix.rhs);
-        }
-        Node::Integer(_) => (),
-        Node::Let(let_) => {
-            locals.insert(let_.lhs.value.clone());
-            if let Some(rhs) = &let_.rhs {
-                visit!(rhs);
-            }
-        }
-        Node::PostfixCall(call) => {
-            visit!(&call.target);
-            for argument in call.arguments.iter() {
-                visit!(argument);
-            }
-        }
-        Node::PostfixProperty(property) => {
-            visit!(&property.target);
-        }
-        Node::Return(ret) => {
-            if let Some(rhs) = &ret.rhs {
-                visit!(rhs);
-            }
-        }
-        Node::String(_) => (),
-        Node::Module(root) => {
-            for node in root.nodes.iter() {
-                visit!(node);
-            }
-        }
-        Node::Var(var) => {
-            locals.insert(var.lhs.value.clone());
-            if let Some(rhs) = &var.rhs {
-                visit!(rhs);
-            }
         }
     }
 }
@@ -374,38 +209,6 @@ impl PostfixProperty {
 #[derive(Clone, Debug, PartialEq)]
 pub struct Module {
     pub nodes: Vec<Node>,
-    /// The root is like a function in that it's evaluated within a frame,
-    /// therefore it needs to have closure bindings like a frame.
-    bindings: Option<HashSet<String>>,
-    /// These should be the union of imports and builtins.
-    parent_bindings: Option<HashSet<String>>,
-}
-
-impl Module {
-    pub fn new(nodes: Vec<Node>) -> Self {
-        let node = Node::Module(Self {
-            nodes,
-            bindings: None,
-            parent_bindings: None,
-        });
-        let (bindings, parent_bindings) = detect_bindings(&node);
-        match node {
-            Node::Module(mut module) => {
-                module.bindings = bindings;
-                module.parent_bindings = parent_bindings;
-                module
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn get_bindings(&self) -> Option<HashSet<String>> {
-        self.bindings.clone()
-    }
-
-    pub fn get_parent_bindings(&self) -> Option<HashSet<String>> {
-        self.parent_bindings.clone()
-    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
